948bb902a18e5d6991f5fb688a64b1bb5300daeb
Sourabh Pramod <psourabh2@gmail.com>
2024-04-22 21:46:50 +0530

    static all products page created, added an add products button

--- routes/index.js
+++ routes/index.js
@@ -74,4 +74,43 @@ router.get('/admin', function (req, res, next) {
   res.render('adminpage', { title: "Seller Login", products })
 })
 
+router.get('/admin/products', function (req, res, next) {
+  let products = [
+    {
+      name: "Iphone 11",
+      category: "Mobile",
+      description: "Rs. 1,20,000",
+      image: "https://www.smartcellular.in/media/catalog/product/cache/cfaa1692031f009488d1cfea5ce7e1ee/r/_/r_1_1.jpg"
+
+    },
+    {
+      name: "OnePlus 7T",
+      category: "Mobile",
+      description: "Rs. 63,940",
+      image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1708679170/Croma%20Assets/Communication/Mobiles/Images/250994_0_wvqdxi.png?tr=w-600"
+
+    },
+    {
+      name: "Redmi Note 10S",
+      category: "Mobile",
+      description: "Rs. 14,900",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDQVUgUtdfLktljb-cAxZ6loAMiQ1UJrU_xZdzH9LmQg&s"
+
+    },
+    {
+      name: "Vivo Y20",
+      category: "Mobile",
+      description: "Rs. 23,000",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0GjGRrJMcF0wCYJvEGKMMiNeT7k5wRzg0kpL0cCV-ag&s"
+
+    },
+
+
+  ]
+
+
+
+  res.render('viewproducts', { title: "Products", products })
+})
+
 module.exports = router;
